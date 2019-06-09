2a2b117d0529683009c58ba90432820ab9a2bdaf
Jakub Krymarys <krymarys.jakub@gmail.com>
2019-06-09 22:00:28 +0200

    connection with external weather data

---
+++ src/homepage/homepage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import axios from "axios";
+import CurrentData from "./curentData";
+
+class Homepage extends React.Component {
+
+
+  render() {
+    return <div>
+      <CurrentData/>
+    </div>
+  }
+}
+
+export default Homepage;
