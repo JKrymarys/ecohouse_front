98d6fe88479eb632fc631d1f91793bf44a45ac8b
Jakub Krymarys <krymarys.jakub@gmail.com>
2019-08-10 15:58:20 +0200

    Updates

---
+++ precache-manifest.5a5e7a771dd675aca5634437200e1524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "200b820257c5ad07512fa173bb63b545",
+    "url": "/ecohouse_front/index.html"
+  },
+  {
+    "revision": "06b4e391668eb0551330",
+    "url": "/ecohouse_front/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "de85ab4d80eb166c8a4b",
+    "url": "/ecohouse_front/static/js/2.fb479b54.chunk.js"
+  },
+  {
+    "revision": "06b4e391668eb0551330",
+    "url": "/ecohouse_front/static/js/main.9d14e145.chunk.js"
+  },
+  {
+    "revision": "051c762a6ad444ffe1d1",
+    "url": "/ecohouse_front/static/js/runtime~main.287288bb.js"
+  }
+]);
